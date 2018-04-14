16d4fee4bc4f0f0104c3d249af3ca7e036d48629
savannah <savannah.m.liu.18@dartmouth.edu>
2018-04-13 21:30:37 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import $ from 'jquery'; // es6
+import './style.scss';
 
 /* set interval https://www.w3schools.com/js/js_timing.asp */
 let num = 0;
-num = setInterval(myTimer, 1000);
 
 function myTimer() {
-  let text = 'You have been on this page for ' + num +' seconds';
+  const text = `You have been on this page for ${num} seconds`;
   $('#main').html(text);
-  num++;
+  num += 1;
 }
+
+num = setInterval(myTimer, 1000);
