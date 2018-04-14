0bd3e622338f8001cf41899787b6208981d4c528
savannah <savannah.m.liu.18@dartmouth.edu>
2018-04-13 21:43:54 -0400

    fixed some things in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import $ from 'jquery'; // es6
 import './style.scss';
 
-/* set interval https://www.w3schools.com/js/js_timing.asp */
 let num = 0;
 
-function myTimer() {
+function seconds() {
   const text = `You have been on this page for ${num} seconds`;
   $('#main').html(text);
   num += 1;
 }
 
-num = setInterval(myTimer, 1000);
+setInterval(seconds, 1000);
